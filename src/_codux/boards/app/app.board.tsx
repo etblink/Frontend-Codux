ddabf4eaffe6e2153e0c970691305b469da96667
etblink <etblink@users.noreply.github.com>
2024-01-04 18:44:49 +0000

    codux

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,7 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../../../App';
 
 export default createBoard({
     name: 'App',
-    Board: () => <App />,
+    Board: () => (
+        <MemoryRouter initialEntries={['/']}>
+            <App />
+        </MemoryRouter>
+    ),
 });
