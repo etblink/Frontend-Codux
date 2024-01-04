f7bef1e4ed346f22af0afd6fd32fea40d181a9bc
etblink <veg4ssnow@gmail.com>
2024-01-04 11:12:55 -0800

    `update'

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,24 @@ import { Header } from './components/header/header';
 import Header_module from './components/header/header.module.scss';
 import Events_module from './components/events/events.module.scss';
 import { Footer } from './components/footer/footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/home-page/home-page';
 import { CommunityPage } from './components/community-page/community-page';
 
 function App() {
     const [count, setCount] = useState(0);
 
-  return (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/community" element={<CommunityPage />} />
-                {/* Add other routes here */}
-            </Routes>
-            <Footer />
-        </div>
-    </BrowserRouter>
+  return (    
+    <div>
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            {/* Add other routes here */}
+        </Routes>
+        <Footer />
+    </div>    
   );
   }
+
   export default App;
